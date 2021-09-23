f7b4959c4c7b1acefd256073760a328aee57f85c
Kilian Perez <kilianperez92@gmail.com>
2021-09-23 10:41:51 +0200

    Arreglando problemas conectase a la Api
    Solventado creándome una cuenta y accediendo con apiKey

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -6,15 +6,31 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class PaisService {
-  private apiUrl: string = 'https://restcountries.com/v2'; // URL de la api
+  // private apiUrl: string = 'https://restcountries.com/v2'; // URL de la api
   // private apiUrl: string = 'https://restcountrxies.eu/rest/v2'; // URL de la api antigua
-  constructor(
-    private http: HttpClient //importamos el servicio de http para las peticiones a la api
-  ) {}
+  // d421c79edca7ee9341ab0587143ce021;
+  // private apiUrl: string =
+  //   'http://api.countrylayer.com/v2/d421c79edca7ee9341ab0587143ce021/';
+  // constructor(
+  //   private http: HttpClient //importamos el servicio de http para las peticiones a la api
+  // ) {}
+
+  // buscarPais(termino: string): Observable<any> {
+  //   const url = `${this.apiUrl}/name/${termino}`;
+
+  //   return this.http.get(url); // si queremos que esto se ejecute tiene que tener un .subscribe() pero la informacion no la queremos mandar dentro del servicio sino al componente que quiere buscar el país
+  // }
+
+  private apiUrl: string = 'http://api.countrylayer.com/v2';
+  private apiKey: string = 'd421c79edca7ee9341ab0587143ce021';
+  constructor(private http: HttpClient) {}
 
   buscarPais(termino: string): Observable<any> {
     const url = `${this.apiUrl}/name/${termino}`;
-
-    return this.http.get(url); // si queremos que esto se ejecute tiene que tener un .subscribe() pero la informacion no la queremos mandar dentro del servicio sino al componente que quiere buscar el país
+    return this.http.get(url, {
+      params: {
+        access_key: this.apiKey,
+      },
+    });
   }
 }
